0473824b90f17be5c1a4a0a4969fd1703d069385
Sergey_Emelyanov <example@gmail.com>
2020-05-18 20:42:41 +0300

    "add file"

---
+++ js/script1.js
@@ -0,0 +1,9 @@
+let num = 300;
+if (num < 49) {
+    console.log("неверно!");
+} else if (num > 100) {
+    console.log("много");
+} else {
+    console.log("верно");
+};
+(num = 50) ? console.log("mnmn") : console.log(",n,mn,");
---
+++ js/script2.js
@@ -0,0 +1,9 @@
+"use strict";
+
+const obj = {
+    a: 30
+};
+obj.a = 10;
+console.log(obj);
+ let a = 16;
+console.log(a);
